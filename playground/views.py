4e7eb29d3064a21f62286e2fa5dc9a9659eb17a0
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2022-09-20 13:37:03 +0100

    update

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,15 @@
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        message= EmailMessage('subject', 'message', 'store@store.com', ['boy@store.com'])
+        message.attach_file('playground/static/images/truck-kun.jpg')
+        message.send()
+
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Adekunle'})
+
+
--- storefront/settings.py
+++ storefront/settings.py
@@ -177,3 +177,12 @@ SIMPLE_JWT = {
 
 
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST= 'localhost'
+EMAIL_HOST_USER= ''
+EMAIL_HOST_PASSWORD= ''
+EMAIL_PORT= 2525
+DEFAULT_FROM_EMAIL= 'from@buy.com'
+
+ADMINS = [
+    ('Rhx', 'admin@buy.com')
+]
