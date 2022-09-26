71b91e3adabd67a7a49c4a469c004b0c2459e3d4
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2022-09-26 22:18:49 +0100

    Update

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,18 @@
-from .tasks import notify_customers
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+import requests
+from rest_framework.views import APIView
 
 
-def say_hello(request):
-    notify_customers.delay('Yo, you good?')
-    return render(request, 'hello.html', {'name': 'Adekunle'})
+class HelloView(APIView):
+
+    @method_decorator(cache_page(5*60))
+    def get(self, request):
+        response = requests.get('http://httpbin.org/delay/2')
+        data = response.json()
+
+        return render(request, 'hello.html', {'name': data})
 
 
